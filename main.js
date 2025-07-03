e4dcb158073d506ea4ffa7a109cfd0498a522a2f
pimentelamanda <164514050+pimentelamanda@users.noreply.github.com>
2025-07-03 13:36:25 +0000

    123

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll ('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho (){
-    if (tamanhoSenha >1){
-        tamanhoSenha --;
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-function aumentaTamanho (){
-    if (tamanhoSenha< 20){
-        tamanhoSenha ++;
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
     }
-    numeroSenha.textContent = tamanhoSenha;
+    campoSenha.value = senha;
 }
