8ff3cc23ab3b90c45787799d66bddf3636cf8115
pimentelamanda <164514050+pimentelamanda@users.noreply.github.com>
2025-08-07 12:38:27 +0000

    123

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector ('.forca')
+const forcaSenha = document.querySelector ('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -60,17 +60,16 @@ function geraSenha() {
     classificaSenha (alfabeto.length);
 }
 
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca','media','forte');
-        
-    if (entropia > 57){
+        if (entropia > 57){
            forcaSenha.classList.add('forte');
-        } else if (entropia > 5 && entropia <= 11) {
+        } else if (entropia > 35 && entropia <= 57) {
             forcaSenha.classList.add('media');
-        } else if (entropia <= 5){
+        } else if (entropia <= 35){
             forcaSenha.classList.add('fraca');
         }
         const valorEntropia = document.querySelector('.entropia');
-        valorEntropia.textContent = "Um computador pode levar até" + Math.floor(2** entropia/(100e6*60*60*24))+ "dias para descobrir essa senha."
+        valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
